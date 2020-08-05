8ef422fef5fbd3889baebf7c29d4e0bc4f0b5ff2
RASTOGI <aayush.rastogi@in.imshealth.com>
2020-08-05 20:30:41 +0530

    Updates

---
+++ precache-manifest.adeed4b302ce8ce645a39b8c2e62a7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448a32133837d25443de932ec52bc3d5",
+    "url": "/VideoUploader/index.html"
+  },
+  {
+    "revision": "9bd5e1a038833da7a486",
+    "url": "/VideoUploader/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "912bde3c98d8def25ca7",
+    "url": "/VideoUploader/static/css/main.d3671be1.chunk.css"
+  },
+  {
+    "revision": "9bd5e1a038833da7a486",
+    "url": "/VideoUploader/static/js/2.801de637.chunk.js"
+  },
+  {
+    "revision": "912bde3c98d8def25ca7",
+    "url": "/VideoUploader/static/js/main.9b519fe5.chunk.js"
+  },
+  {
+    "revision": "ad72c30552972e0b3151",
+    "url": "/VideoUploader/static/js/runtime~main.18224075.js"
+  }
+]);
